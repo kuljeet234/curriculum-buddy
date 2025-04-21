5ec60b26ad35777ccb9ae41558de50a34f535635
kuljeet234 <kuljeet.229310234@muj.manipal.edu>
2025-04-21 20:45:10 +0530

    Remove config.py from tracking

--- config.py
+++
@@ -1,4 +0,0 @@
-# config.py
-GEMINI_API_KEY = "AIzaSyAzfZOa-tII5-ZQrDEJibVnS7FQYH-7DNo"
-FILE_PATH = "/Users/kuljeetsinghshekhawat/Downloads/ML(AI3201)_Course Handout.pdf"
-SERP_API_KEY = "0a8a760d1d0b544e300db0c45b21cf6f904223ffca25d9373109926f5222efd5"
